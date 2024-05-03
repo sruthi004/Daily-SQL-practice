9abe523b611e44e31ebd875314c8cd2657c05e93
SS <sruthirajpandian@gmail.com>
2024-05-03 15:30:00 -0400

    Python

---
+++ 05 - May/03 May - Tesla Models.py
@@ -0,0 +1,6 @@
+# AnalystBuilder
+# https://www.analystbuilder.com/questions/tesla-models-soJdJ
+
+import pandas as pd;
+tesla_models['profit'] = (tesla_models['car_price']-tesla_models['production_cost'])*tesla_models['cars_sold']
+tesla_models[tesla_models['profit']==tesla_models['profit'].max()]
