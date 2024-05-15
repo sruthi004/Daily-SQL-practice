a728bb7b3dc61761fbb5b06f44744e1078afb993
SS <sruthirajpandian@gmail.com>
2024-05-15 15:53:57 -0400

    Python

---
+++ 05 - May/15 May - Pepperoni-flation.py
@@ -0,0 +1,6 @@
+# AnalystBuilder
+# https://www.analystbuilder.com/questions/pepperoni-flation-PleiU
+
+import pandas as pd;
+orders = orders[orders['pizza_order']=='Pepperoni']
+orders['pizza_order'].count()*0.25
